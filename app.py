1ceec665266feab941baa227eca98990d9e46aad
msaniyafirdouse <msaniyafirdouse.23is@saividya.ac.in>
2026-02-25 11:23:29 +0530

    comment

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ def analyze():
         "classification": classification,
         "reasons": reasons
     })
-
+#final line
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
